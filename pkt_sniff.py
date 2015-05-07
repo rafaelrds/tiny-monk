f2bc5bef4c2fe01a0d8595f726fcfd36df5eb9af
rafaelrds <crafards@gmail.com>
2015-05-07 17:12:01 +0200

    Representation middly modified. Webbrowser stuff in the way

--- pkt_sniff.py
+++ pkt_sniff.py
@@ -3,16 +3,16 @@ from scapy.all import *
 import datetime
 
 def diff_time_in_miliseconds(t1, t2):
-    t1 = datetime.datetime.strptime(t1, '%H:%M:%S.%f').time()
-    t2 = datetime.datetime.strptime(t2, '%H:%M:%S.%f').time()
-    h1, m1, s1, mm1 = t1.hour, t1.minute, t1.second, t1.microsecond
-    h2, m2, s2, mm2 = t2.hour, t2.minute, t2.second, t2.microsecond
-    t1_secs = s1 + 60 * (m1 + 60*h1)
-    t2_secs = s2 + 60 * (m2 + 60*h2)
-    t1_msecs = (t1_secs * int(1e6)) + mm1
-    t2_msecs = (t2_secs * int(1e6)) + mm2
-    delta = t2_msecs - t1_msecs
-    return (delta)/1000.0 if delta > 0 else (24*60*60*int(1e6) + delta)/1000.0
+  t1 = datetime.datetime.strptime(t1, '%H:%M:%S.%f').time()
+  t2 = datetime.datetime.strptime(t2, '%H:%M:%S.%f').time()
+  h1, m1, s1, mm1 = t1.hour, t1.minute, t1.second, t1.microsecond
+  h2, m2, s2, mm2 = t2.hour, t2.minute, t2.second, t2.microsecond
+  t1_secs = s1 + 60 * (m1 + 60*h1)
+  t2_secs = s2 + 60 * (m2 + 60*h2)
+  t1_msecs = (t1_secs * int(1e6)) + mm1
+  t2_msecs = (t2_secs * int(1e6)) + mm2
+  delta = t2_msecs - t1_msecs
+  return (delta)/1000.0 if delta > 0 else (24*60*60*int(1e6) + delta)/1000.0
 
 def max_response_time(list_of_dnspairs):
   max_rtime = 0.0
@@ -41,7 +41,8 @@ class DNS_pair_stamped(object):
   def __repr__(self):
     print 50*"*"
     print "Transaction ID:", self.pkt_q[DNS].id
-    print "Query:", self.time_q, self.pkt_q[DNS].qd.qname
+    print "Query time:", self.time_q
+    print "Query name:", self.pkt_q[DNS].qd.qname
     print "Response:", self.time_r
     print "Response time", diff_time_in_miliseconds(self.time_q, self.time_r),"ms"
     print 50*"*"
@@ -82,25 +83,28 @@ def select_DNS(pkt):
 
       del(DNS_DICT[dns_id])
       DNS_PAIRS.append(dns_pair_stamped)
-      print 'a'
       print dns_pair_stamped
-      print 'b'
 
   except:
     print "An exception was throwed!"
   
 # ------ START SNIFFER ------
 import webbrowser
-url = 'http://www.utwente.nl/'
+url = 'http://www.nu.nl'
 
 # Open URL in a new tab, if a browser window is already open.
-# webbrowser.get('firefox').open_new_tab('http://www.google.com')
-# webbrowser.open_new_tab(url + 'onderwijs/')
-
+# webbrowser.get('firefox').open(url)
+webbrowser.get('firefox').open(url)
 # Open URL in new window, raising the window if possible.
 # webbrowser.open_new(url)
+import platform
+plat = platform.platform().lower()
+if 'darwin' in plat:
+  print "MAC OS"
+elif 'linux' in plat:
+  print "LINUX"
 
-sniff(iface=interface, filter=filter_bpf, store=0,  prn=select_DNS, timeout=60)
+sniff(iface=interface, filter=filter_bpf, store=0,  prn=select_DNS, timeout=30)
 
 # ------ ANALYSIS ------
 total_time = 0
@@ -111,10 +115,22 @@ for p in DNS_PAIRS:
 
 print "Total of %d packets" % (packetCount)
 print "Total of %d pairs" % (len(DNS_PAIRS))
-print "Maximum response time %.3fms" % (max_response_time(DNS_PAIRS))
-print "Minimum response time %.3fms" % (min_response_time(DNS_PAIRS))
-print "Time between first Q and last R %.3fms" % (calculate_total_dnsload(DNS_PAIRS))
-print "Sum of QR time %.3fms" % (total_time)
-print "Average of response time %.6fms" % (total_time/(packetCount/2.0))
- 
+print "Maximum response time %.3f ms" % (max_response_time(DNS_PAIRS))
+print "Minimum response time %.3f ms" % (min_response_time(DNS_PAIRS))
+print "Time between first Q and last R %.3f ms" % (calculate_total_dnsload(DNS_PAIRS))
+print "Sum of QR time %.3f ms" % (total_time)
+print "Average of response time %.6f ms" % (total_time/(packetCount/2.0))
+print "Orfan packets: %d" % (len(DNS_DICT.keys()))
+
+
+
+
+
+
+
+
+
+
+
+
 
